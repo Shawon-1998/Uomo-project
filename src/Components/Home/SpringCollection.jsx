c3000d54a7a4f77f4071cd0e9d3450093782ce5f
shawon-1998 <billalhossainshawon989@gmail.com>
2026-05-04 12:55:37 +0600

    added countdown part by installing npm package react count down and layout handle by daisy ui

---
+++ src/Components/Home/SpringCollection.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import Container from '../Ui/Container'
+import { Link } from 'react-router'
+import Countdown from 'react-countdown';
+import Image from '../common/Image'
+import fragrance from '../../assests/images/Fragrance.jpg'
+const SpringCollection = () => {
+    const renderer = ({ days, hours, minutes, seconds, completed }) => {
+
+        return (
+
+            < div className="grid grid-flow-col gap-5 text-center auto-cols-max" >
+                <div className="flex flex-col">
+                    <span className="countdown font-mono text-5xl">
+                        {days}
+                    </span>
+                    days
+                </div>
+                <div className="flex flex-col">
+                    <span className="countdown font-mono text-5xl">
+                        {hours}
+                    </span>
+                    hours
+                </div>
+                <div className="flex flex-col">
+                    <span className="countdown font-mono text-5xl">
+                        {minutes}
+                    </span>
+                    min
+                </div>
+                <div className="flex flex-col">
+                    <span className="countdown font-mono text-5xl">
+                        {seconds}
+                    </span>
+                    sec
+                </div>
+            </div >
+        )
+
+    };
+
+    return (
+        <>
+            <section className='bg-[#EBEBEB] '>
+                <Container>
+                    <main className=' flex justify-between items-center'>
+                        <div className='pt-52.25 pb-9'>
+                            <div className='flex gap-5 items-center'>
+                                <div className='w-10 h-0.5 bg-secondary-red' />
+                                <p className='text-secondary-red font-jost font-medium text-sm'>DEAL OF THE WEEK</p>
+                            </div>
+                            <div>
+                                <h1 className='font-jost text-[70px] text-primary-black uppercase mt-2.25 mb-2.75'><span className='font-bold'>Spring</span> Collection</h1>
+                            </div>
+                            <div className="font-jost text-sm font-medium mb-30.25 "  >
+                                <Link className='hoverItems '>SHOP NOW</Link>
+                            </div>
+                            <Countdown
+                                date={Date.now() + (3600000 * 24 * 16)}
+                                renderer={renderer}
+                            />
+                        </div>
+                        <Image src={fragrance} alt="frarance" className="w-106.5 h-135.75   " />
+                    </main>
+                </Container>
+            </section>
+        </>
+    )
+}
+
+export default SpringCollection
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner3 from '../Components/Home/Banner3'
 import Category from '../Components/Home/category/Category'
 import TrendyProducts from '../Components/Home/TrendyProducts'
+import SpringCollection from '../Components/Home/SpringCollection'
 
 
 
@@ -11,6 +12,7 @@ const Home = () => {
       <Banner3 />
       <Category />
       <TrendyProducts />
+      <SpringCollection />
     </>
   )
 }
