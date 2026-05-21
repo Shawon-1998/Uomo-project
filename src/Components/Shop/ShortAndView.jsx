2e850920c3c432356e70aed0b7d29e5e168e6a27
shawon-1998 <billalhossainshawon989@gmail.com>
2026-05-21 14:21:56 +0600

    decrease filter gap

--- src/Components/Shop/ShortAndView.jsx
+++ src/Components/Shop/ShortAndView.jsx
@@ -6,7 +6,7 @@ const ShortAndView = ({ filteredProducts }) => {
     return (
         <section>
 
-            <div className='flex justify-between lg:gap-15.5 lg:mb-10 gap-63.5 mb-5 items-center'>
+            <div className='flex justify-between lg:gap-15.5 lg:mb-10 gap-39.5 mb-5 items-center'>
                 <div className='after:bg-primary-black relative leading-6 after:absolute after:bottom-0 after:left-0 after:h-0.5   after:content-[""] after:w-full'>
                     <select className='text-primary-black font-jost font-medium! text-sm border-none outline-none lg:pe-6 pe-2 uppercase' name="" id="">
                         <option className='text-primary-black!'
